73d09388892b9f000f20d5e6fc536bc4391b0667
chrsearl <chris.searle@finn.no>
2013-11-29 13:22:08 +0100

    Style embedded 500px and flickr images for responsive

--- helpers/photo_helper.rb
+++ helpers/photo_helper.rb
@@ -1,7 +1,7 @@
 module PhotoHelper
   def embed_500px(id, image_url, title)
     %Q{<a href="http://500px.com/photo/#{id}">
-        <img src="#{image_url}" alt="#{title} on 500px.com" border="0" style="margin: 0 0 5px 0;" width="853">
+        <img class="embedimg" src="#{image_url}" alt="#{title} on 500px.com" border="0">
       </a>
       <br/>
       <font style="font-size: 120%;">
@@ -16,7 +16,9 @@ module PhotoHelper
   end
   
   def embed_flickr(id, image_url, title)
-    %Q{<a href="http://www.flickr.com/photos/chrissearle/#{id}" title="#{title} by Chris Searle, on Flickr"><img src="#{image_url}" width="853" alt="#{title}"></a>}
+    %Q{<a href="http://www.flickr.com/photos/chrissearle/#{id}" title="#{title} by Chris Searle, on Flickr">
+         <img class="embedimg" src="#{image_url}" alt="#{title}">
+       </a>}
   end
 
   def link_flickr(id, title)
