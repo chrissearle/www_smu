81a5bd7f746680ca5e93a0a5b0e01378d8945cc2
chrsearl <chris.searle@finn.no>
2013-11-29 13:11:36 +0100

    Make youtube vid's responsive

--- helpers/youtube_helper.rb
+++ helpers/youtube_helper.rb
@@ -1,5 +1,5 @@
 module YoutubeHelper
   def youtube(yid)
-    %Q{<iframe width="853" height="480" src="//www.youtube.com/embed/#{yid}" frameborder="0" allowfullscreen></iframe>}
+    %Q{<div class="flex-video widescreen"><iframe src="//www.youtube.com/embed/#{yid}" frameborder="0" allowfullscreen></iframe></div>}
   end
 end
