35330eb73aa0f5329e0985d3913a37abd855eb9c
Chris Searle <chris@chrissearle.org>
2014-09-22 14:35:11 +0200

    Use appropriate helpers

--- helpers/flytrex_helper.rb
+++ helpers/flytrex_helper.rb
@@ -1,5 +1,10 @@
 module FlytrexHelper
-  def flytrex(fid, title)
+  def flytrex_phantom(fid, title)
     %Q{<a href="http://www.flytrex.com/mission/quadcopter-phantom-chris-searle-#{fid}">#{title} on flytrex.com</a>}
   end
+
+  def flytrex_f550(fid, title)
+    %Q{<a href="http://www.flytrex.com/mission/hexacopter-f550-chris-searle-#{fid}">#{title} on flytrex.com</a>}
+  end
 end
+
