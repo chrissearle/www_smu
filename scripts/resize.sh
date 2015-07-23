ce1613144bbb4dbbeda64627d13082455af51b65
Chris Searle <chris@chrissearle.org>
2015-07-23 17:12:55 +0200

    oops - images too big

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-mkdir -p original
-mkdir -p converted
-
-for FILE in *jpg; do
-	echo $FILE
-	convert $FILE -resize 1000x1000\> converted/$FILE
+for F in `find $1 -name *.jpg -print`; do
+  width=`identify -format %w $F`
+  if [ $width -gt 1000 ]; then
+    echo $F Too big
+    convert $F -resize 1000x1000\> $F.new
+    rm $F
+    mv $F.new $F
+  fi
 done
-
-cd converted
-
-for FILE in *jpg; do
-	mv ../$FILE ../original/$FILE
-	mv $FILE ../$FILE
-done
-
-cd ..
-
-rmdir converted
