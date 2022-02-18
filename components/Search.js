6d18b846635f98f712ae6038b8a46162d4fe093f
Chris Searle <chris@chrissearle.org>
2022-02-18 23:02:05 +0100

    Add search

---
+++ components/Search.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function Search() {
+  return (
+    <div className="my-2 card">
+      <div className="card-body">
+        <div className="card-text">
+          <form action="//www.google.com" id="cse-search-box">
+            <input
+              type="hidden"
+              name="cx"
+              value="partner-pub-6208435820744910:5135878443"
+            />
+            <input type="hidden" name="ie" value="UTF-8" />
+            <input type="text" name="q" className="form-control" />
+            <input
+              type="submit"
+              name="sa"
+              className="form-control"
+              value="Search"
+            />
+          </form>
+          <script src="//www.google.com/coop/cse/brand?form=cse-search-box&amp;lang=en" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,12 @@
 import Browse from "./Browse";
 import LatestArticles from "./LatestArticles";
 import PopularTags from "./PopularTags";
+import Search from "./Search";
 
 export default function Sidebar({ files }) {
   return (
     <div className="sidebar">
+      <Search />
       <Browse />
       <PopularTags files={files} />
       <LatestArticles files={files} />
