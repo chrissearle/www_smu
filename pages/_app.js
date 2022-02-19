832d4f2c2535ba2cf40e5537b96aed89ab053720
Chris Searle <chris@chrissearle.org>
2022-02-19 09:26:15 +0100

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Layout from "../components/Layout";
+
+import { pageview } from "../lib/ga";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "prismjs/themes/prism-solarizedlight.css";
 import "../styles/overrides.css";
 
-import Layout from "../components/Layout";
-
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout files={pageProps.files}>
       <Component {...pageProps} />
