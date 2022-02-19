400b7193e61db8ac00a63c11d0cb54d59c96f565
Chris Searle <chris@chrissearle.org>
2022-02-19 16:18:27 +0100

    Move handling of old categories to redirects

--- next.config.js
+++ next.config.js
@@ -4,4 +4,23 @@ module.exports = {
   experimental: {
     outputStandalone: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/categories/rc",
+        destination: "/categories/Radio%20Control",
+        permanent: true,
+      },
+      {
+        source: "/categories/3dp",
+        destination: "/categories/3D%20Printing",
+        permanent: true,
+      },
+      {
+        source: "/categories/photo",
+        destination: "/categories/Photography",
+        permanent: true,
+      },
+    ];
+  },
 };
--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -40,11 +40,8 @@ export async function getStaticPaths() {
       .flat()
   );
 
-  // Handle the old names
-  const allCategories = [...uniqueCategories, "rc", "photo", "3dp"];
-
   return {
-    paths: allCategories.map((category) => ({
+    paths: [...uniqueCategories].map((category) => ({
       params: { category: category },
     })),
     fallback: false,
@@ -53,17 +50,6 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   let cat = params.category;
-
-  // Handle the old names
-  if (cat === "rc") {
-    cat = "Radio Control";
-  }
-  if (cat === "3dp") {
-    cat = "3D Printing";
-  }
-  if (cat === "photo") {
-    cat = "Photography";
-  }
 
   const files = loadMarkdown({ reverse: true });
 
