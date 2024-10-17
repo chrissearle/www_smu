e8788a451cc014b8272f46532acd612b46b27584
Chris Searle <chris@chrissearle.org>
2024-10-17 12:17:39 +0200

    Hydration does not like certain elements inside p tags. Seems to be mostly img for now - so make sure content doesn't wrap img in p

--- app/components/content/ProseP.vue
+++ app/components/content/ProseP.vue
@@ -1,3 +1,13 @@
+<script setup>
+const slots = useSlots()
+
+// Hacky check for an image
+const isImage = computed(() => {
+  return slots.default()[0].type.tag === 'img'
+})
+</script>
+
 <template>
-  <p class="my-2"><slot /></p>
+  <div class="prose-image" v-if="isImage"><slot /></div>
+  <p class="my-2" v-else><slot /></p>
 </template>
