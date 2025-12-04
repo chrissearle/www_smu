35ed0f514a66edd3eb07c2c4f417f3f806f33fe5
Chris Searle <chris@chrissearle.org>
2025-12-04 07:10:09 +0100

    Update keys layout

--- app/pages/keys.vue
+++ app/pages/keys.vue
@@ -8,86 +8,64 @@
 
     <h2 class="sectionTitle">Current</h2>
 
-      <div class="d-flex flex-column ga-6 ma-6">
-        <v-card>
-          <v-card-title>
-            <h4 class="text-h4">Current</h4>
-          </v-card-title>
+    <p class="mb-2">Get the public keys direct from the keyservers.</p>
 
-          <v-card-subtitle>
-            Get the public keys direct from the keyservers.
-            You can also check the proofs on my
-            <a
-                href="https://keyoxide.org/116FBA8295065A01C3BC7CC6CF79BF54D4BF0A41"
-                target="_blank"
-                rel="noopener noreferer"
-            >
-              keyoxide profile
-            </a>
-          </v-card-subtitle>
+    <p class="mb-2">You can also check the proofs on my
+      <a
+          href="https://keyoxide.org/116FBA8295065A01C3BC7CC6CF79BF54D4BF0A41"
+          target="_blank"
+          rel="noopener noreferer"
+      >
+        keyoxide profile.
+      </a>
+    </p>
 
-          <v-card-text>
-            <h5 class="text-h5">Work (BB80AB21)</h5>
+    <h3 class="subSectionTitle">Work (BB80AB21)</h3>
 
-            <div class="fingerprint">
-              Fingerprint: FF6E 6FFA C4CB 0727 38D0 E1EB 4E5F 28B9 BB80 AB21
-            </div>
-          </v-card-text>
+    <div class="fingerprint">
+      <pre>Fingerprint: FF6E 6FFA C4CB 0727 38D0 E1EB 4E5F 28B9 BB80 AB21</pre>
+    </div>
 
-          <v-card-text>
-            <h5 class="text-h5">Private (D4BF0A41)</h5>
+    <h3 class="subSectionTitle">Private (D4BF0A41)</h3>
 
-            <div class="fingerprint">
-              Fingerprint: 116F BA82 9506 5A01 C3BC 7CC6 CF79 BF54 D4BF 0A41
-            </div>
-          </v-card-text>
+    <div class="fingerprint">
+      <pre>Fingerprint: 116F BA82 9506 5A01 C3BC 7CC6 CF79 BF54 D4BF 0A41</pre>
+    </div>
 
-          <v-card-text>
-            These replace the older two keys and are both using a larger key size
-            and SHA-256 instead of SHA-1.
+    <p class="mb-2">
+      These replace the older two keys and are both using a larger key size
+      and SHA-256 instead of SHA-1.
 
-            <a
-                href="http://ekaia.org/blog/2009/05/10/creating-new-gpgkey/"
-                target="_blank"
-                rel="noopener noreferer"
-            >This blog post by Ana Beatriz Guerrero López</a>
-            was very clear on how to generate these new keys.
-          </v-card-text>
-        </v-card>
+      <a
+          href="http://ekaia.org/blog/2009/05/10/creating-new-gpgkey/"
+          target="_blank"
+          rel="noopener noreferer"
+      >This blog post by Ana Beatriz Guerrero López</a>
+      was very clear on how to generate these new keys.
+    </p>
 
-        <h2 class="sectionTitle">Outdated</h2>
+    <h2 class="sectionTitle">Outdated</h2>
 
-        <v-card>
-          <v-card-title>
-            <h4 class="text-h4">Outdated</h4>
-          </v-card-title>
+    <h3 class="subSectionTitle">Outdated</h3>
 
-          <v-card-subtitle>
-            These keys are no longer in use - but are published to help showing the history
-          </v-card-subtitle>
+    <p class="mb-2">
+      These keys are no longer in use - but are published to help showing the history
+    </p>
 
-          <v-card-text>
-            <h5 class="text-h5">Work (83C1EAEB)</h5>
+    <h3 class="subSectionTitle">Work (83C1EAEB)</h3>
 
-            <p class="my-2">Replaced by BB80AB21</p>
+    <p class="mb-2">Replaced by BB80AB21</p>
 
-            <div class="fingerprint">
-              Fingerprint: 0D89 45A9 A5CD 15C4 FF77 7D8D 8FEA 08A2 83C1 EAEB
-            </div>
-          </v-card-text>
+    <div class="fingerprint">
+      <pre>Fingerprint: 0D89 45A9 A5CD 15C4 FF77 7D8D 8FEA 08A2 83C1 EAEB</pre>
+    </div>
 
-          <v-card-text>
-            <h5 class="text-h5">Home/Scouts (224A5434)</h5>
+    <h3 class="subSectionTitle">Home/Scouts (224A5434)</h3>
 
-            <p class="my-2">Replaced by D4BF0A41</p>
+    <p class="mb-2">Replaced by D4BF0A41</p>
 
-            <div class="fingerprint">
-              Fingerprint: A4AE 46C6 EA6C DD12 CC57 623F BF50 786C 224A 5434
-            </div>
-          </v-card-text>
-        </v-card>
-      </div>
+    <div class="fingerprint">
+      <pre>Fingerprint: A4AE 46C6 EA6C DD12 CC57 623F BF50 786C 224A 5434</pre>
+    </div>
   </UContainer>
 </template>
-<script setup lang="ts">
-</script>
